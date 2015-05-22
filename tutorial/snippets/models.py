95c5b7b8351b1604d2662c5305ccbd29c8b7e0ae
CDanM <danmbugua74@gmail.com>
2015-05-22 09:31:58 +0300

    Database and class setup
    Synchronized the database and created a serializer class.

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_lexers
+from pygments.styles import get_all_styles
 
-LEXRS =[item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([item[1][0],item[0] for item in LEXERS])
+LEXERS =[item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0],item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item,item) for item in get_all_lexers())
 
 class Snippet(models.Model):
